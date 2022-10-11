5da69470efc560dddf857ae393b6c594a38bd208
Ishan <ishandutta2007@gmail.com>
2022-10-11 13:26:20 +0530

    ascii

--- tts_offline_demo.py
+++ tts_offline_demo.py
@@ -1,6 +1,8 @@
 import pyttsx3
-with open ("data.txt", "r") as myfile:
+with open ("data.txt", "r", encoding="utf8") as myfile:
     text = myfile.read().splitlines()
+
 engine = pyttsx3.init('sapi5')
+# engine.setProperty('rate',145)
 engine.say(text)
 engine.runAndWait()
